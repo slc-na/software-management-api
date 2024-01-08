0a8162780ffdc460e4a9befc78dd998868c3f437
AnthonyChandraa <anthonychd.22@gmail.com>
2024-01-08 10:26:04 +0700

    Update Course Model

--- src/modules/courses/courses.model.ts
+++ src/modules/courses/courses.model.ts
@@ -1,15 +1,19 @@
 import { Field, GraphQLISODateTime, ObjectType } from "@nestjs/graphql";
-import { Course as CourseDB } from "@prisma/client"
+import { Course as CourseDB, InternetUsageType } from "@prisma/client"
 import { SoftwareCourse } from "../software-courses/software-courses.model";
+import { Department } from "../departments/departments.model";
 
 @ObjectType()
 export class Course {
   @Field(() => String) id: CourseDB['id'];
   @Field(() => String) code: CourseDB['code']
   @Field(() => String) name: CourseDB['name'];
+  @Field(() => Department) department: Department;
   @Field(() => String) departmentId: CourseDB['departmentId'];
+  @Field(() => String) internetUsageType: InternetUsageType;
   @Field(() => String) internetUsageTypeId: CourseDB['internetUsageTypeId'];
   @Field(() => GraphQLISODateTime) createdAt: CourseDB['createdAt'];
   @Field(() => GraphQLISODateTime) updatedAt: CourseDB['updatedAt'];
   @Field(() => [SoftwareCourse]) softwareCourses: SoftwareCourse[];
+  
 }
