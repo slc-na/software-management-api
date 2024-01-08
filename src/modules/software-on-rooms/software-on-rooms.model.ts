807245229b7ec442bd48997360d0f80b42d9660b
Anthony William Chandra <75880728+AnthonyChandraa@users.noreply.github.com>
2024-01-08 16:01:54 +0700

    Fix Models Many To One Relationship

--- src/modules/software-on-rooms/software-on-rooms.model.ts
+++ src/modules/software-on-rooms/software-on-rooms.model.ts
@@ -1,10 +1,15 @@
 import { Field, GraphQLISODateTime, ObjectType } from "@nestjs/graphql";
-import { SoftwareOnRoom as SoftwareOnRoomDB } from "@prisma/client";
+import { Room, SoftwareOnRoom as SoftwareOnRoomDB } from "@prisma/client";
+import { Software } from "../softwares/softwares.model";
+import { Semester } from "../semesters/semesters.model";
 
 @ObjectType()
 export class SoftwareOnRoom {
+  @Field(() => Software) software: Software;
   @Field(() => String) softwareId: SoftwareOnRoomDB['softwareId'];
+  @Field(() => String) room: Room;
   @Field(() => String) roomId: SoftwareOnRoomDB['roomId'];
+  @Field(() => Semester) semester: Semester
   @Field(() => String) semesterId: SoftwareOnRoomDB['semesterId'];
   @Field(() => GraphQLISODateTime) createdAt: SoftwareOnRoomDB['createdAt'];
   @Field(() => GraphQLISODateTime) updatedAt: SoftwareOnRoomDB['updatedAt'];
